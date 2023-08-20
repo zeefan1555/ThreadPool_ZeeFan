947fb25f6da028f5a9c8a22a6576988f8d50c089
zeefan <1145468360@qq.com>
2023-08-20 16:37:02 +0800

    v1.1: 用函数指针来传递函数当成参数

--- threadpool.cpp
+++ threadpool.cpp
@@ -1,35 +1,25 @@
-// 线程池项目-最终版.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
 #include <iostream>
-#include <functional>
 #include <thread>
-#include <future>
-#include <chrono>
-using namespace std;
 
-#include "threadpool.h"
-
-
-
-
-int sum1(int a, int b)
+class ZeefanExecutorV1
 {
-    this_thread::sleep_for(chrono::seconds(2));
-    // 比较耗时
-    return a + b;
-}
-int sum2(int a, int b, int c)
+public:
+    void execute(void(*func)())
+    {
+        std::thread(func).detach();
+    }
+};
+
+void myFuntion()
 {
-    this_thread::sleep_for(chrono::seconds(2));
-    return a + b + c;
+    std::cout<<"thread running" << std::endl;
 }
+
 
 int main()
 {
-   thread t1{sum1, 1,2};
-   thread t2{sum2, 1,2,3};
-   t1.join();
-   t2.join();
+    ZeefanExecutorV1 obj;
+    obj.execute(myFuntion);
 
+    return 0;
 }
